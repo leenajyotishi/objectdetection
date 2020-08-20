26fdf87e9d6758164e9a0ce7ec847718926d4908
leenajyotishi <69509483+leenajyotishi@users.noreply.github.com>
2020-08-20 17:17:54 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,28 +1,41 @@
-# Flask based wordcloud generator
-This is a flask based app to scrap user reviews and comments from a retail website and generates word-cloud with CSV data.
-The data that was gathered is available to download as well.
+## ML-Model-Flask-Deployment
+This is a demo project to elaborate how Machine Learn Models are deployed on production using Flask API
 
-This could be a way to showcase a buyer that what in general other consumers are feeling about that product.
+### Prerequisites
+You must have Scikit Learn, Pandas (for Machine Leraning Model) and Flask (for API) installed.
 
-**For live demo you can check here-** http://c17hawke-scraper01.herokuapp.com/
+### Project Structure
+This project has four major parts :
+1. model.py - This contains code fot our Machine Learning model to predict employee salaries absed on trainign data in 'hiring.csv' file.
+2. app.py - This contains Flask APIs that receives employee details through GUI or API calls, computes the precited value based on our model and returns it.
+3. request.py - This uses requests module to call APIs already defined in app.py and dispalys the returned value.
+4. templates - This folder contains the HTML template to allow user to enter employee detail and displays the predicted employee salary.
 
-## Following is some screenshots from the project -
-### 1. Home page with a search bar to search a product.
+### Running the project
+1. Ensure that you are in the project home directory. Create the machine learning model by running below command -
+```
+python model.py
+```
+This would create a serialized version of our model into a file model.pkl
 
-![Home page](https://github.com/c17hawke/peronal-blog-2020/blob/master/project_screenshots/flask-based-wordcloud-generator/homePage.png?raw=true)
----
-### 2. Page to display the reviews in a tablular form. From here you can download the data as well.
+2. Run app.py using below command to start Flask API
+```
+python app.py
+```
+By default, flask will run on port 5000.
 
-![Results page](https://github.com/c17hawke/peronal-blog-2020/blob/master/project_screenshots/flask-based-wordcloud-generator/search_results.png?raw=true)
----
-### 3. To see the wordcloud generated you can click on wordcloud tab in the navbar that will open a new page with wordcloud generated with buyer's comment. This gives a brief idea about the product.
+3. Navigate to URL http://localhost:5000
 
-![Wordcloud display](https://github.com/c17hawke/peronal-blog-2020/blob/master/project_screenshots/flask-based-wordcloud-generator/wordcloud.png?raw=true)
----
-### 4. 404 page will be seen if you enter something gibrish or product is not spelled properly.
+You should be able to view the homepage as below :
+![alt text](http://www.thepythonblog.com/wp-content/uploads/2019/02/Homepage.png)
 
-![404 error page](https://github.com/c17hawke/peronal-blog-2020/blob/master/project_screenshots/flask-based-wordcloud-generator/404.png?raw=true)
+Enter valid numerical values in all 3 input boxes and hit Predict.
 
-### 5. View on my cellphone -
+If everything goes well, you should  be able to see the predcited salary vaule on the HTML page!
+![alt text](http://www.thepythonblog.com/wp-content/uploads/2019/02/Result.png)
 
-![view on phone](https://github.com/c17hawke/peronal-blog-2020/blob/master/project_screenshots/flask-based-wordcloud-generator/view_on_phone.jpg?raw=true)
+4. You can also send direct POST requests to FLask API using Python's inbuilt request module
+Run the beow command to send the request with some pre-popuated values -
+```
+python request.py
+```
--- app.py
+++ app.py
@@ -1,209 +1,37 @@
-'''
-# Author: Sunny Bhaveen Chandra
-# Contact: sunny.c17hawke@gmail.com
-# dated: March, 04, 2020
-'''
-# import necessary libraries
-from bs4 import BeautifulSoup as soup
-import urllib
-import requests
-import pandas as pd
-import time
-import os
-from flask import Flask, render_template,  session, redirect, request
-from flask_cors import CORS,cross_origin
-import matplotlib.pyplot as plt
-from wordcloud import WordCloud, STOPWORDS
-
-# define global paths for Image and csv folders
-IMG_FOLDER = os.path.join('static', 'images')
-CSV_FOLDER = os.path.join('static', 'CSVs')
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
 
 app = Flask(__name__)
-# config environment variables
-app.config['IMG_FOLDER'] = IMG_FOLDER
-app.config['CSV_FOLDER'] = CSV_FOLDER
+model = pickle.load(open('model.pkl', 'rb'))
 
-class DataCollection:
-	'''
-	class meant for collection and management of data
-	'''
-	def __init__(self):
-		# dictionary to gather data
-		self.data = {"Product": list(), 
-		"Name": list(),
-		"Price (INR)": list(), 
-		"Rating": list(), 
-		"Comment Heading": list(), 
-		"Comment": list()}
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-	def get_final_data(self, commentbox=None, prodName=None, prod_price=None):
-		'''
-		this will append data gathered from comment box into data dictionary
-		'''
-		# append product name
-		self.data["Product"].append(prodName)
-		self.data["Price (INR)"].append(prod_price)
-		try:
-			# append Name of customer if exists else append default
-			self.data["Name"].append(commentbox.div.div.\
-				find_all('p', {'class': '_3LYOAd _3sxSiS'})[0].text)
-		except:
-			self.data["Name"].append('No Name')
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
 
-		try:
-			# append Rating by customer if exists else append default
-			self.data["Rating"].append(commentbox.div.div.div.div.text)
-		except:
-			self.data["Rating"].append('No Rating')
+    output = round(prediction[0], 2)
 
-		try:
-			# append Heading of comment by customer if exists else append default
-			self.data["Comment Heading"].append(commentbox.div.div.div.p.text)
-		except:
-			self.data["Comment Heading"].append('No Comment Heading')
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
-		try:
-			# append comments of customer if exists else append default
-			comtag = commentbox.div.div.find_all('div', {'class': ''})
-			self.data["Comment"].append(comtag[0].div.text)
-		except:
-			self.data["Comment"].append('')	
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
 
-	def get_main_HTML(self, base_URL=None, search_string=None):
-		'''
-		return main html page based on search string
-		'''
-		# construct the search url with base URL and search string
-		search_url = f"{base_URL}/search?q={search_string}"
-		# usung urllib read the page
-		with urllib.request.urlopen(search_url) as url:
-			page = url.read()
-		# return the html page after parsing with bs4
-		return soup(page, "html.parser")
+    output = prediction[0]
+    return jsonify(output)
 
-	def get_product_name_links(self, flipkart_base=None, bigBoxes=None):
-		'''
-		returns list of (product name, product link)
-		'''
-		# temporary list to return the results
-		temp = []
-		# iterate over list of bigBoxes
-		for box in bigBoxes:
-			try:
-				# if prod name and list present then append them in temp
-				temp.append((box.div.div.div.a.img['alt'],
-					flipkart_base + box.div.div.div.a["href"]))
-			except:
-				pass
-			
-		return temp
-
-	def get_prod_HTML(self, productLink=None):
-		'''
-		returns each product HTML page after parsing it with soup
-		'''
-		prod_page = requests.get(productLink)
-		return soup(prod_page.text, "html.parser")
-
-
-	def get_data_dict(self):
-		'''
-		returns collected data in dictionary
-		'''
-		return self.data
-
-	def save_as_dataframe(self, dataframe, fileName=None):
-		'''
-		it saves the dictionary dataframe as csv by given filename inside
-		the CSVs folder and returns the final path of saved csv
-		'''
-		# save the CSV file to CSVs folder
-		csv_path = os.path.join(app.config['CSV_FOLDER'], fileName)
-		fileExtension = '.csv'
-		final_path = f"{csv_path}{fileExtension}"
-		# clean previous files -
-		CleanCache(directory=app.config['CSV_FOLDER'])
-		# save new csv to the csv folder
-		dataframe.to_csv(final_path, index=None)
-		print("File saved successfully!!")
-		return final_path
-
-
-	def save_wordcloud_image(self, dataframe=None, img_filename=None):
-		'''
-		it generates and saves the wordcloud image into wc_folder
-		'''
-		# extract all the comments
-		txt = dataframe["Comment"].values
-		# generate the wordcloud
-		wc = WordCloud(width=800, height=400, background_color='black', stopwords=STOPWORDS).generate(str(txt))
-
-		plt.figure(figsize=(20,10), facecolor='k', edgecolor='k')
-		plt.imshow(wc, interpolation='bicubic') 
-		plt.axis('off')
-		plt.tight_layout()
-		# create path to save wc image
-		image_path = os.path.join(app.config['IMG_FOLDER'], img_filename + '.png')
-		# Clean previous image from the given path
-		CleanCache(directory=app.config['IMG_FOLDER'])
-		# save the image file to the image path
-		plt.savefig(image_path)
-		plt.close()
-		print("saved wc")
-
-
-class CleanCache:
-	'''
-	this class is responsible to clear any residual csv and image files
-	present due to the past searches made.
-	'''
-	def __init__(self, directory=None):
-		self.clean_path = directory
-		# only proceed if directory is not empty
-		if os.listdir(self.clean_path) != list():
-			# iterate over the files and remove each file
-			files = os.listdir(self.clean_path)
-			for fileName in files:
-				print(fileName)
-				os.remove(os.path.join(self.clean_path,fileName))
-		print("cleaned!")
-
-# route to display the home page
-@app.route('/',methods=['GET'])  
-@cross_origin()
-def homePage():
-	return render_template("index.html")
-
-# route to display the review page
-@app.route('/upload')
-@cross_origin()
-def index():
-	if request.method == 'POST':
-		try:
-			print('Test')
-		except Exception as e:
-			print(e)
-			
-			return render_template("404.html")
-
-	else:
-		# return index page if home is pressed or for the first run
-		return render_template("index.html")
-
-# route to display wordcloud
-@app.route('/show')  
-@cross_origin()
-def show_wordcloud():
-	img_file = os.listdir(app.config['IMG_FOLDER'])[0]
-	full_filename = os.path.join(app.config['IMG_FOLDER'], img_file)
-	return render_template("show_wc.html", user_image = full_filename)
-# route to display wordcloud
-@app.route('/upload')  
-
-def show_upload():
-	img_file = os.listdir(app.config['IMG_FOLDER'])[0]
-	full_filename = os.path.join(app.config['IMG_FOLDER'], img_file)
-	return render_template("show_wc.html", user_image = full_filename)
-if __name__ == '__main__':
-	app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
